fc15b4f20bc7c79a0c0c4e94c75fa65fa8c9a955
Felipe S.C. Lorentz <felipesclorentz+github@gmail.com>
2024-01-30 16:08:01 -0300

    DB

--- server.ts
+++ server.ts
@@ -1,41 +1,31 @@
 import { Application, Context, send } from 'https://deno.land/x/oak/mod.ts';
 import { bold, cyan, green, yellow } from "https://deno.land/std@0.200.0/fmt/colors.ts";
-import { indexedDB } from "https://deno.land/x/indexeddb@v1.1.0/ponyfill.ts";
-//
-import file from "./database/chat.json" with { type: "json" };
-const filePath = './database/chat.json';
+import { DB } from "https://deno.land/x/sqlite/mod.ts";
 
 const port = 8080;
-const app = new Application({ keys: ["secret1"] });
+const app = new Application({ keys: ["data"] });
+const db = new DB('./database/data.db');
 
-app.use(async (context, next) => {
-  if (context.request.method === 'POST') {
-    const body = await context.request.body().value;
-    context.response.body = 'Recebido com sucesso!\n';
-    try {
-      const jsonData = typeof body === 'string' ? JSON.parse(body) : body;
-      const existingData = file.find(item => item.id === jsonData.id);
-      if (existingData) {
-        // Atualiza o valor se o id já existe no arquivo
-        existingData.value = jsonData.value;
-        console.log('Dado atualizado:', existingData);
-      } else {
-        // Adiciona o novo dado ao array no arquivo JSON
-        file.push(jsonData);
-        console.log('Novo dado adicionado:', jsonData);
-      }
-      // Salva as alterações de volta no arquivo
-      await Deno.writeTextFile(filePath, JSON.stringify(file));
-      context.response.body = 'Recebido e armazenado com sucesso!\n';
-    } catch (error) {
-      console.error('Erro ao processar JSON:', error);
-      context.response.status = 400;
-      context.response.body = 'Erro ao processar JSON.\n';
-    }
-  } else {
-    await next();
-  }
-});
+db.execute(`
+  CREATE TABLE IF NOT EXISTS people (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    value TEXT
+  )
+`);
+
+// Run a simple query
+/*for (const name of ["Peter Parker", "Clark Kent", "Bruce Wayne"]) {
+  db.query("INSERT INTO people (name) VALUES (?)", [name]);
+}*/
+
+// Print out data in table
+for (const [name] of db.query("SELECT name FROM people")) {
+  console.log(name);
+}
+
+// Close connection
+db.close();
 
 app.use(async (context) => {
   try {
