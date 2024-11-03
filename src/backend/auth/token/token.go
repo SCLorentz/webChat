5139eca3521b6a4b1b245f5b0135e2583e38b7aa
Felipe S.C. Lorentz <113468900+SCLorentz@users.noreply.github.com>
2024-11-03 19:22:43 +0000

    .

--- src/backend/auth/token/token.go
+++ src/backend/auth/token/token.go
@@ -3,26 +3,12 @@ package token
 import (
     "fmt"
     "time"
-    "github.com/dgrijalva/jwt-go"
 	"github.com/google/uuid"
 )
 
-func GenerateToken(userID int) (string, error) {
-    claims := jwt.StandardClaims{
-        ExpiresAt: time.Now().Add(time.Hour * 24).Unix(),
-        Issuer:    "ch6",
-        Subject:   fmt.Sprintf("%d", userID),
-    }
-
-    token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-
-    ss, err := token.SignedString([]byte(uuid.New().String()))
-    if err != nil {
-        return "", err
-    }
-
-    return ss, nil
-}
+/*func GenerateToken(userID int) (string, error) {
+    
+}*/
 
 /*func ValidateToken(tokenString string) (int, error) {
     
--- src/backend/go.mod
+++ src/backend/go.mod
@@ -2,9 +2,9 @@ module webchat
 
 go 1.22.5
 
+require github.com/google/uuid v1.6.0
+
 require (
-	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
-	github.com/google/uuid v1.6.0 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/mattn/go-sqlite3 v1.14.24 // indirect
 )
